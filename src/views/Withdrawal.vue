9ca5c58cd5487aa1ae528f54b32876a3a84e1676
Achilles45 <achillesusuoyibo7@gmail.com>
2020-06-12 15:45:59 -0700

    Changed the phone number and email to suit

--- src/views/Withdrawal.vue
+++ src/views/Withdrawal.vue
@@ -36,7 +36,7 @@
              <small>You can now make request to make withdrawal into your local bank when your investment has reached maturity</small>
              <hr>
               <div v-if="verifyuser == 'false'" class="red">
-                  You can't make withdrawals because your account or payment has not been verified. Please contact admin or call <a href="tel:07061116827">07061116827</a>
+                  You can't make withdrawals because your account or payment has not been verified. Please contact admin or call <a href="tel:08106864691">08106864691</a>
               </div>
                <div class="row">
                    <div class="col-md-8">
